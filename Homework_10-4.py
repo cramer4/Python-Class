edb9279e0c6ced5d9f62c2aa7dccffa7ee871ff9
cramer4 <73566217+cramer4@users.noreply.github.com>
2021-02-02 16:44:19 -0500

    Delete Homework_10-4.py

--- Homework_10-4.py
+++
@@ -1,9 +0,0 @@
-def get_names():
-
-    with open("guest_list.txt", "a") as file_object:
-        while True:
-            name = input('What is your name? ("q" to quit): ')
-            if name != "q":
-                file_object.write(f"{name}\n")
-            else: break
-get_names()
